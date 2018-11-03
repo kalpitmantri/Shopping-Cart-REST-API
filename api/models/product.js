86f1459a7ff1ad0fb32c85f9beb425693cd38074
Kalpit <kalpitmantri98@gmail.com>
2018-11-03 18:59:39 +0530

    ProductRoutes updated!

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    price: Number
+    name: {type:String,required:true},
+    price: {type:Number,required:true}
 });
 
 const Product = mongoose.model('Product', productSchema);
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -5,10 +5,26 @@ const Product = require('../models/product');
 
 router.get('/',(req,res,next)=>{
 	Product.find()
+		   .select('_id name price')
 		   .exec()
 		   .then(docs=>{
 		   		console.log(docs);
-		   		res.status(200).json(docs);
+		   		const response = {
+		   			count:docs.length,
+		   			products:docs.map(doc=>{
+		   				return {
+		   					_id:doc._id,
+		   					name:doc.name,
+		   					price:doc.price,
+		   					request:{
+		   						type:'GET',
+		   						descripption:'Get details of the product.',
+		   						 url: "http://localhost:3000/products/" + doc._id
+		   					}
+		   				}
+		   			})
+		   		}
+		   		res.status(200).json(response);
 		   })
 		   .catch(err=>{
 		   		console.log(err);
@@ -29,8 +45,17 @@ router.post('/',(req,res,next)=>{
 	.then(result => {
 		console.log(result);
 		res.status(201).json({
-			product : product,
-			message : 'Post route for products'
+			message : 'Product Created Successfully!!',
+			createdProduct:{
+				_id:result._id,
+				name:result.name,
+				price:result.price,
+				request:{
+					type:"GET",
+					description:"Get details of the product",
+					url:'http://localhost:3000/products/' + result._id
+				}
+			}
 		});
 	})
 	.catch(err=>{
@@ -44,11 +69,19 @@ router.post('/',(req,res,next)=>{
 router.get('/:productId',(req,res,next)=>{
 	var id = req.params.productId;
 	Product.findById(id)
+		   .select('_id name price')
 		   .exec()
 		   .then(doc=>{
 		   		console.log(doc);
 		   		if(doc){
-		   			res.status(200).json(doc);
+		   			res.status(200).json({
+		   				product:doc,
+		   				request:{
+		   					type:'GET',
+		   					description:'Get list of all products',
+		   					url:'http://localhost:3000/products'
+		   				}
+		   			});
 		   		}
 		   		else{
 		   			res.status(404).json({
@@ -73,7 +106,14 @@ router.patch('/:productId',(req,res,next)=>{
 	Product.update({_id:id},{$set:updateOps})
 		   .exec()
 		   .then(result=>{
-		   		res.status(200).json(result);
+		   		res.status(200).json({
+		   			message:"Product Updated Successfully",
+		   			request:{
+		   				type:'GET',
+		   				description:'Get product details.',
+		   				url:'http://localhost:3000/products' + id
+		   			}
+		   		});
 		   })
 		   .catch(err=>{
 		   		console.log(err);
@@ -91,7 +131,18 @@ router.delete('/:productId',(req,res,next)=>{
 	Product.remove({_id:id})
 		   .exec()
 		   .then(result =>{
-		   		res.status(200).json(result);
+		   		res.status(200).json({
+		   			message:'Product Deleted',
+		   			request:{
+		   				type:'POST',
+		   				description:'Create new Product',
+		   				url:'http://localhost:3000/products',
+		   				body:{
+		   					name:'String',
+		   					price:'Number'
+		   				}
+		   			}
+		   		});
 		   })
 		   .catch(err=>{
 		   		console.log(err);
