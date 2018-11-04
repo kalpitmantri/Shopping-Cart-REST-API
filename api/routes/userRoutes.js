866becc3ca225244470ba5d33ba17f04deeed136
Kalpit <kalpitmantri98@gmail.com>
2018-11-04 09:59:15 +0530

    User login and delete added

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -50,4 +50,68 @@ router.post('/signup',(req,res,next)=>{
 		});
 });
 
+router.delete('/:userId',(req,res,next)=>{
+	User.remove({_id:req.params.userId})
+		.exec()
+		.then(result=>{
+			res.status(200).json({
+				message:'User successfully deleted!!'
+			})
+		})
+		.catch(err=>{
+			console.log(err);
+			res.status(500).json({
+				error:err
+			});
+		});
+});
+
+router.post('/login',(req,res,next)=>{
+	User.find({email:req.body.email})
+		.exec()
+		.then(user=>{
+			if(user.length < 1){
+				return res.status(401).json({
+					message:'Auth Failed!!'
+				});
+			}
+			else{
+				bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+					if(err){
+						// console.log(err);		
+						return res.status(401).json({
+							message:'Auth Failed!!'
+						});
+					}
+					if(result){
+						const token = jwt.sign(
+							{
+								email:user[0].email,
+								Id:user[0]._id
+							},
+							'secret',
+							{
+								expiresIn:'1h'
+							}
+						);
+
+						return res.status(200).json({
+							message:'Auth Successful!!',
+							token:token
+						});
+					}
+					return res.status(401).json({
+						message:'Auth Failed!!'
+					});
+				});
+			}
+		})
+		.catch(err=>{
+			console.log(err);
+			res.status(500).json({
+				error:err
+			});
+		});
+});
+
 module.exports = router;
